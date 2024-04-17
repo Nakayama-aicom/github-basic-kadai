5e853dcdb7af7c11a2113d1683660c8eb0b8782b
Nakayama-aicom <167046764+Nakayama-aicom@users.noreply.github.com>
2024-04-17 13:14:31 +0900

    kadai_018をアップロード

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,14 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18{
+	
+	public void setGivenName(String hanako){
+		this.givenName = hanako;
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("趣味は読書です\n");
+	}
+
+
+}
---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18 {
+	
+	public void setGivenName(String taro){
+		this.givenName = taro;
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("Javaが得意です\n");
+	}
+
+}
